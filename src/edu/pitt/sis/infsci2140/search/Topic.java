286a11b4ddd43235a1ecbe57c55e9ec2d78b535a
HugoZhang33 <zhanghong.gdufs@gmail.com>
2015-03-10 00:42:56 -0400

    finished Topic

--- src/edu/pitt/sis/infsci2140/search/Topic.java
+++ src/edu/pitt/sis/infsci2140/search/Topic.java
@@ -20,90 +20,93 @@ import edu.pitt.sis.infsci2140.analysis.TextTokenizer;
  * A class that stores topic information.
  */
 public class Topic {
-	private ArrayList<char[]> titleInfo = new ArrayList<char[]>();
-	private String topicId;
-	
-	public Topic(String topicId) {
-		this.topicId = topicId;
-	}
-	
-	public String topicId() {
-		// you should implement this method
-		return topicId;
-	}
-	
-	public void setTopicId(String id) {
-		this.topicId = id;
-	}
-	
-	/**
-	 * Parse a list of TREC topics from the provided f.
-	 * 
-	 * @param f
-	 * @return
-	 */
-	public static List<Topic> parse( File f ) {
-		// you should implement this method
-		
-		ArrayList<Topic> queryList = new ArrayList<Topic>();
-		TextTokenizer tokenizer = null;
-		int id = 0;
-		
-		// only pick up title for query
-		try {
-			BufferedReader reader = new BufferedReader( new InputStreamReader( new FileInputStream(f), "UTF-8" ) );
-			BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( f.getParent() + "/Query.txt" ), "UTF-8" ) );
-
-			String line = reader.readLine();
-			
-			while ( line != null ) {
-				if ( line.indexOf( "<title>" ) != -1 ) { // find it, extract the content
-					int start = 7;
-					char[] content = line.substring(start).toCharArray();
-					
-					// tokenize and normalize title info
-					// add to Topic instance's titleInfo list
-					writer.write( String.valueOf(id) );
-					writer.write(' ');
-					
-					Topic topic = new Topic( String.valueOf( id++ ) );
-					tokenizer = new TextTokenizer( content );
-					char[] term = tokenizer.nextWord();
-					
-					while ( term != null ) {
-						term = TextNormalizer.normalize(term);
-						topic.setTitleInfo(term);
-						writer.write(term);
-						writer.write(' ');
-					}
-					queryList.add(topic);
-					writer.write('\n');
-				}
-				line = reader.readLine();
-			}
-			
-			reader.close();
-			writer.close();
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		return queryList;
-	}
-
-	public ArrayList<char[]> getTitleInfo() {
-		return titleInfo;
-	}
-
-	public void setTitleInfo(char[] title) {
-		this.titleInfo.add(title);
-	}
-	
+    private ArrayList<char[]> titleInfo = new ArrayList<char[]>();
+    private String topicId;
+    
+    public Topic(String topicId) {
+        this.topicId = topicId;
+    }
+    
+    public String topicId() {
+        // you should implement this method
+        return topicId;
+    }
+    
+    public void setTopicId(String id) {
+        this.topicId = id;
+    }
+    
+    /**
+     * Parse a list of TREC topics from the provided f.
+     *
+     * @param f
+     * @return
+     */
+    public static List<Topic> parse( File f ) {
+        // you should implement this method
+        
+        ArrayList<Topic> queryList = new ArrayList<Topic>();
+        TextTokenizer tokenizer = null;
+        int id = 0;
+        
+        // only pick up title for query
+        try {
+            BufferedReader reader = new BufferedReader( new InputStreamReader( new FileInputStream(f), "UTF-8" ) );
+            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( "Query.txt" ), "UTF-8" ) );
+            
+            String line = reader.readLine();
+            
+            while ( line != null ) {
+                if ( line.indexOf( "<title>" ) != -1 ) { // find it, extract the content
+                    int start = 7;
+                    char[] content = line.substring(start).toCharArray();
+                    
+                    // tokenize and normalize title info
+                    // add to Topic instance's titleInfo list
+                    writer.write( String.valueOf(id) );
+                    writer.write(' ');
+                    
+                    Topic topic = new Topic( String.valueOf( id++ ) );
+                    tokenizer = new TextTokenizer( content );
+                    char[] term = tokenizer.nextWord();
+                    
+                    while ( term != null ) {
+                        term = TextNormalizer.normalize(term);
+                        topic.setTitleInfo(term);
+                        
+                        writer.write(term);
+                        writer.write(' ');
+                        
+                        term = tokenizer.nextWord();
+                    }
+                    queryList.add(topic);
+                    writer.write('\n');
+                }
+                line = reader.readLine();
+            }
+            
+            reader.close();
+            writer.close();
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return queryList;
+    }
+    
+    public ArrayList<char[]> getTitleInfo() {
+        return titleInfo;
+    }
+    
+    public void setTitleInfo(char[] title) {
+        this.titleInfo.add(title);
+    }
+    
 }
